035e5068f2a4ca8711145ff5bb120c3b71b29c05
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2024-08-28 13:57:01 +0300

    Remove a word to test protected branch 2nd

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,5 +1,5 @@
 public class WordCount {
     public static void main(String[] args) {
-        System.out.println("Word Count Program Main");
+        System.out.println("Word Count Program");
     }
 }
