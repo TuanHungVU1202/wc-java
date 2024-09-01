d53ac877c241941fd7a629ac91b2bca6c2bc3ae3
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2024-09-01 16:16:06 +0300

    Revert "Add print out to debug test"
    This reverts commit 556a293b49d55ea0a751d58081247b9288a034b4.

--- src/test/java/service/SizeCounterTests.java
+++ src/test/java/service/SizeCounterTests.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SizeCounterTests {
     @Test
     public void shouldReturnMinusOneWhenInputIsNullForGetSizeInByteMethod() {
-        System.out.println("shouldReturnMinusOneWhenInputIsNullForGetSizeInByteMethod");
         long expectedSize = -1;
         long actualSize = SizeCounter.getSizeInByte(null);
         assertEquals(expectedSize, actualSize);
