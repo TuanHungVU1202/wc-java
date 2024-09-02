6b5487c0bcf0954d24c9e070bc93a8c209628a3f
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2024-09-02 17:50:00 +0300

    Add tests for SizeCounter class

--- src/test/java/service/SizeCounterTests.java
+++ src/test/java/service/SizeCounterTests.java
@@ -2,13 +2,52 @@ package service;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SizeCounterTests {
+    String m_TestFileStr = "src/test/resources/test.txt";
+
     @Test
     public void shouldReturnMinusOneWhenInputIsNullForGetSizeInByteMethod() {
         long expectedSize = -1;
         long actualSize = SizeCounter.getSizeInByte(null);
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void shouldReturnMinusOneWhenInputIsNullForGetSizeInKiloBytesMethod() {
+        long expectedSize = -1;
+        long actualSize = SizeCounter.getSizeInKiloBytes(null);
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void shouldReturnMinusOneWhenInputIsNullForGetSizeInMegabytesMethod() {
+        long expectedSize = -1;
+        long actualSize = SizeCounter.getSizeInMegabytes(null);
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void shouldReturnCorrectSizeInBytesWhenInputFileExists() {
+        long expectedSize = 342190;
+        long actualSize = SizeCounter.getSizeInByte(new File(m_TestFileStr));
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void shouldReturnCorrectSizeInKiloBytesWhenInputFileExists() {
+        long expectedSize = 342190 / 1024;
+        long actualSize = SizeCounter.getSizeInKiloBytes(new File(m_TestFileStr));
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void shouldReturnCorrectSizeInMegabytesWhenInputFileExists() {
+        long expectedSize = 342190 / (1024 * 1024);
+        long actualSize = SizeCounter.getSizeInMegabytes(new File(m_TestFileStr));
         assertEquals(expectedSize, actualSize);
     }
 }
