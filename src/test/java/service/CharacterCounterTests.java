4f6a6a11ce121eb0ed72b71da17b93e16b3ef8e5
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2024-09-04 20:12:07 +0300

    Add tests for CharacterCounter class

---
+++ src/test/java/service/CharacterCounterTests.java
@@ -0,0 +1,85 @@
+package service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import org.junit.jupiter.api.Test;
+
+public class CharacterCounterTests {
+    @Test
+    public void shouldReturnMinusOneWhenInputIsNull() {
+        int characterCount = CharacterCounter.countCharacters(null);
+        assertEquals(-1, characterCount);
+    }
+
+    @Test
+    public void shouldCountCharactersInFile() throws Exception {
+        File file = new File("src/test/resources/test.txt");
+        Process process = Runtime.getRuntime().exec(new String[]{"wc", "-c", file.getAbsolutePath()});
+        process.waitFor();
+        String output = new String(process.getInputStream().readAllBytes()).trim();
+        int expectedCharacterCount = Integer.parseInt(output.split("\\s+")[0]);
+        
+        int characterCount = CharacterCounter.countCharacters(file);
+        assertEquals(expectedCharacterCount, characterCount);
+    }
+
+    @Test
+    public void shouldReturnNegativeOneForNonExistentFile() throws Exception {
+        File nonExistentFile = File.createTempFile("non_existent", ".txt");
+        nonExistentFile.delete(); // Ensure the file doesn't exist
+        int characterCount = CharacterCounter.countCharacters(nonExistentFile);
+        assertEquals(-1, characterCount);
+    }
+
+    @Test
+    public void shouldReturnNegativeOneForDirectory() {
+        File directory = new File("src/test/resources");
+        int characterCount = CharacterCounter.countCharacters(directory);
+        assertEquals(-1, characterCount);
+    }
+
+    @Test
+    public void shouldCountCharactersInEmptyFile() throws Exception {
+        File emptyFile = File.createTempFile("empty", ".txt");
+        emptyFile.deleteOnExit();
+        int characterCount = CharacterCounter.countCharacters(emptyFile);
+        assertEquals(0, characterCount);
+    }
+
+    @Test
+    public void shouldCountCharactersInSingleCharacterFile() throws Exception {
+        File singleCharFile = File.createTempFile("single_char", ".txt");
+        singleCharFile.deleteOnExit();
+        Files.write(singleCharFile.toPath(), "a".getBytes());
+        int characterCount = CharacterCounter.countCharacters(singleCharFile);
+        assertEquals(1, characterCount);
+    }
+
+    @Test
+    public void shouldCountCharactersInMultiLineFileIncludingNewLines() throws Exception {
+        File multiLineFile = File.createTempFile("multi_line", ".txt");
+        multiLineFile.deleteOnExit();
+        Files.write(multiLineFile.toPath(),
+                "This is a test.\nIt has multiple lines.\nTotal 59 characters.".getBytes());
+        int characterCount = CharacterCounter.countCharacters(multiLineFile);
+        assertEquals(59, characterCount);
+    }
+
+    @Test
+    public void shouldHandleLargeFilesWithoutRunningOutOfMemory() throws Exception {
+        int expectedCharacterCount = 1000000;
+        File largeFile = File.createTempFile("large", ".txt");
+        largeFile.deleteOnExit();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < expectedCharacterCount; i++) {
+            builder.append("a");
+        }
+        Files.write(largeFile.toPath(), builder.toString().getBytes());
+
+        int actualCharacterCount = CharacterCounter.countCharacters(largeFile);
+        assertEquals(expectedCharacterCount, actualCharacterCount);
+    }
+}
